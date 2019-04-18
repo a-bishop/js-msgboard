6e62db83edd52d90ff55c017746e139f366361ce
a-bishop <andrew.bishop53@gmail.com>
2019-04-18 00:54:22 -0700

    tried out React's new Hooks API with Msg component

--- app_server/components/Msg.jsx
+++ app_server/components/Msg.jsx
@@ -1,153 +1,115 @@
 const React = require("react");
 
-class Msg extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleMessage = this.handleMessage.bind(this);
-    this.state = {
-      message: this.props.msg,
-      doneEditing: true
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleMessage = this.handleMessage.bind(this);
-    this.cancelEditing = this.cancelEditing.bind(this);
-  }
+const Msg = props => {
+  const [message, handleMsgChange] = React.useState(props.msg);
+  const [doneEditing, isDoneEditing] = React.useState(true);
 
-  handleMessage(event) {
+  function handleMessage(event) {
     event.preventDefault();
     if (event.target.id === "edit") {
-      this.setState({
-        message: this.props.msg,
-        doneEditing: false
-      });
+      handleMsgChange(props.msg), isDoneEditing(false);
     }
-    let id = this.props.id;
-    let action = event.target.id;
-    this.props.handleMsgCallback(
+    const id = props.id;
+    const action = event.target.id;
+    props.handleMsgCallback(
       id,
       action,
-      this.props.userName,
-      this.state.message,
-      this.props.msgEmail
+      props.userName,
+      message,
+      props.msgEmail
     );
   }
 
-  handleChange(event) {
-    this.setState({
-      message: event.target.value
-    });
-  }
+  const cancelButton = (
+    <button
+      id="cancel"
+      className="btn btn-warning mr-3 mb-1"
+      onClick={() => isDoneEditing(true)}
+    >
+      Cancel
+    </button>
+  );
 
-  cancelEditing() {
-    this.setState({
-      doneEditing: true
-    });
-  }
+  const editButton = (
+    <button
+      id="edit"
+      className="btn btn-primary mr-3 mb-1"
+      onClick={handleMessage}
+    >
+      Edit
+    </button>
+  );
 
-  render() {
-    const cancelButton = (
-      <button
-        id="cancel"
-        className="btn btn-warning mr-3 mb-1"
-        onClick={this.cancelEditing}
-      >
-        Cancel
-      </button>
+  const deleteButton = (
+    <button id="delete" className="btn btn-danger mb-1" onClick={handleMessage}>
+      Delete
+    </button>
+  );
+
+  const updateButton = (
+    <button
+      type="submit"
+      id="update"
+      className="btn btn-primary"
+      onClick={handleMessage}
+    >
+      Update
+    </button>
+  );
+
+  let editOrCancelButton = editButton;
+
+  if (props.id === props.messageEditable && !doneEditing) {
+    editOrCancelButton = cancelButton;
+  }
+  let userActions;
+  if (props.isLoggedOut) {
+    userActions = null;
+  } else if (props.msgName === props.userName) {
+    userActions = (
+      <td className="col-3 col-lg-2">
+        <div className="input-group">
+          {editOrCancelButton}
+          {deleteButton}
+        </div>
+      </td>
     );
-    const editButton = (
-      <button
-        id="edit"
-        className="btn btn-primary mr-3 mb-1"
-        onClick={this.handleMessage}
-      >
-        Edit
-      </button>
-    );
-    const deleteButton = (
-      <button
-        id="delete"
-        className="btn btn-danger mb-1"
-        onClick={this.handleMessage}
-      >
-        Delete
-      </button>
-    );
-    const updateButton = (
-      <button
-        type="submit"
-        id="update"
-        className="btn btn-primary"
-        onClick={this.handleMessage}
-      >
-        Update
-      </button>
-    );
-    let editOrCancelButton = editButton;
-    if (
-      this.props.id === this.props.messageEditable &&
-      !this.state.doneEditing
-    ) {
-      editOrCancelButton = cancelButton;
-    }
-    let userActions;
-    if (this.props.isLoggedOut) {
-      userActions = null;
-    } else if (this.props.msgName === this.props.userName) {
-      userActions = (
-        <td className="col-3 col-lg-2">
-          <div className="input-group">
-            {editOrCancelButton}
-            {deleteButton}
-          </div>
-        </td>
-      );
-    } else if (
-      this.props.userName === "Admin" &&
-      this.props.msgName !== this.props.userName
-    ) {
-      userActions = <td className="col-3 col-lg-2">{deleteButton}</td>;
-    } else {
-      userActions = <td className="col-3 col-lg-2" />;
-    }
-    let tableRow = (
+  } else if (props.userName === "Admin" && props.msgName !== props.userName) {
+    userActions = <td className="col-3 col-lg-2">{deleteButton}</td>;
+  } else {
+    userActions = <td className="col-3 col-lg-2" />;
+  }
+  let tableRow = (
+    <tr className="d-flex">
+      <td className="col-3">{props.msgName}</td>
+      <td className={props.isLoggedOut ? "col-9 col-lg-10" : "col-6 col-lg-7"}>
+        {props.msg}
+      </td>
+      {userActions}
+    </tr>
+  );
+  if (props.id === props.messageEditable && !doneEditing) {
+    tableRow = (
       <tr className="d-flex">
-        <td className="col-3">{this.props.msgName}</td>
-        <td
-          className={
-            this.props.isLoggedOut ? "col-9 col-lg-10" : "col-6 col-lg-7"
-          }
-        >
-          {this.props.msg}
+        <td className="col-3">{props.msgName}</td>
+        <td className="col-6 col-lg-7">
+          <form>
+            <textarea
+              id="message"
+              type="text"
+              className="form-control mr-3 mb-1"
+              placeholder="Edit Message"
+              value={message}
+              onChange={() => handleMsgChange(event.target.value)}
+            />
+            {updateButton}
+          </form>
         </td>
         {userActions}
       </tr>
     );
-    if (
-      this.props.id === this.props.messageEditable &&
-      !this.state.doneEditing
-    ) {
-      tableRow = (
-        <tr className="d-flex">
-          <td className="col-3">{this.props.msgName}</td>
-          <td className="col-6 col-lg-7">
-            <form>
-              <textarea
-                id="message"
-                type="text"
-                className="form-control mr-3 mb-1"
-                placeholder="Edit Message"
-                value={this.state.message}
-                onChange={this.handleChange}
-              />
-              {updateButton}
-            </form>
-          </td>
-          {userActions}
-        </tr>
-      );
-    }
-    return <>{tableRow}</>;
   }
-}
+  return <>{tableRow}</>;
+};
 
 module.exports = Msg;
